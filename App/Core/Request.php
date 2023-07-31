fe911f4c07781da5a96857c6efaacfcc0cbcb032
Ali <ali@ali.php>
2023-07-31 15:29:45 +0330

    base of routing - request clas

---
+++ App/Core/Request.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Core;
+
+class Request{
+    private $agent;
+    private $params;
+    private $ip;
+    private $method;
+    private  $uri;
+
+    public function __construct() {
+        $this->agent = $_SERVER['HTTP_USER_AGENT'];
+        $this->params = $_REQUEST;
+        $this->ip = $_SERVER['REMOTE_ADDR'];
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->uri = $_SERVER['REQUEST_URI'];
+    }
+    public function getip()
+    {
+        return $this->ip;
+    } 
+    public function getagent()
+    {
+        return $this->agent;
+    }
+    public function getparams()
+    {
+        return $this->params;
+    }
+    public function getmethod()
+    {
+        return $this->method;
+    }
+    public function geturi()
+    {
+        return $this->uri;
+    }
+    public function isset($key)
+    {
+        return isset($this->params[$key]);
+    }
+    public function redirect()
+    {
+        //header("location : ". funtion to redirect); redirect using helper function.
+        die;
+    }
+}
